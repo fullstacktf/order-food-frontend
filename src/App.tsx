756bc87a9ab6efae67b25458e169c0d40fc5f07e
Nicolás Cabrera <jncabdom@gmail.com>
2021-12-12 18:43:23 +0000

    Quick fixx to App.tsxc

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import { Routes } from "react-router";
 import { BrowserRouter, Link, Route } from "react-router-dom";
 import './main.css'
-import { Error404 } from "./components/pages/Error404";
-import { OrderSummary } from "./components/pages/OrderSummary";
+import { OrderSummary } from "./pages/OrderSummary/OrderSummary";
 
 const App = () => (
   <div>
@@ -11,8 +10,7 @@ const App = () => (
       <Routes>
         <Route path="/"></Route>
         <Route path="/summary" element={<OrderSummary />}></Route>
-        <Route path="/404"></Route>
-        <Route path="*" element={ <Error404/> }></Route>
+        <Route path="*"></Route>
       </Routes>
         <Link to="/summary">Summary</Link>
     </BrowserRouter>
