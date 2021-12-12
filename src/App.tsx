0385747cf1892ed3a976b77e7c22e67ab6f688ff
Nicolás Cabrera <jncabdom@gmail.com>
2021-12-12 18:31:04 +0000

    Creating side navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
+import styled from "@emotion/styled";
 import React from "react";
 import { Routes } from "react-router";
-import { BrowserRouter, Link, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar/Navbar";
 import './main.css'
-import { Error404 } from "./components/pages/Error404";
-import { OrderSummary } from "./components/pages/OrderSummary";
+import { Error404 } from "./pages/Error404";
+import { OrderSummary } from "./pages/OrderSummary/OrderSummary";
+
+const MainApp = styled.div`
+display: flex;
+margin: 0;
+`
 
 const App = () => (
-  <div>
+  <MainApp>
     <BrowserRouter>
+    <Navbar></Navbar>
       <Routes>
         <Route path="/"></Route>
         <Route path="/summary" element={<OrderSummary />}></Route>
         <Route path="/404"></Route>
         <Route path="*" element={ <Error404/> }></Route>
       </Routes>
-        <Link to="/summary">Summary</Link>
     </BrowserRouter>
-  </div>
+  </MainApp>
 );
 
 export default App;
