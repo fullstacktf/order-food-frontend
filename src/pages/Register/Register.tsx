7125da1c90c9c2c8b8ee5b7b721d7fe1a04d814a
Nicolás Cabrera <jncabdom@gmail.com>
2021-12-14 14:39:24 +0000

    Login Screen looking better. Added validation...

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,58 +1,189 @@
-import styled from '@emotion/styled';
-import { TextField } from '@mui/material';
+import styled from '@emotion/styled'
+import {
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+} from '@mui/material'
 import React from 'react'
-import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "yup";
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
-const Form = styled.form`
-margin: 0;
-display: flex;
-justify-content: center;
-align-items: center;
-flex-direction: column;
-height: 100vh;
+const Container = styled.div`
+  margin: 0;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
-const schema = yup.object().shape({
-  firstName: yup.string().required(),
-  lastName: yup.string().required(),
-  age: yup.number().positive().integer().required(),
-}).required();
+const Column = styled.div`
+  flex-direction: column;
+  display: flex;
+  width: 50%;
+  margin: 5px;
+`
+
+// Ew. Find another solution to this!
+const LeftColumn = styled(Column)`
+  padding-top: 13px;
+`
+
+const InnerContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`
+
+const Form = styled.form`
+  width: 60%;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+`
+
+const TextFieldStyled = styled(TextField)`
+  width: 100%;
+  margin-top: 10px;
+`
+
+const Selector = styled(Select)`
+  width: 100%;
+`
+
+const schema = yup
+  .object()
+  .shape({
+    name: yup.string().required('Name is a required field'),
+    email: yup.string().email().required('Email is a required field'),
+    phone: yup
+      .string()
+      .matches(
+        RegExp(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})$/),
+        'Invalid phone number'
+      ),
+    password: yup.string().required('Password is a required field'),
+    street: yup.string().required('Street is a required field'),
+    streetNumber: yup.string().required('Street Number is a required field'),
+    city: yup.string().required('City is a required field'),
+    country: yup.string().required('Country is a required field'),
+    postalCode: yup.string().required('Postal Code is a required field'),
+  })
+  .required()
 
 export const Register = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm({
+  const [role, setRole] = React.useState('user')
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: yupResolver(schema),
-  });
-  const onSubmit = (data: any) => console.log(data);
+  })
+  const onSubmit = (data: any) => {
+    // validate address, complex stuff
+    console.log(data)
+  }
+
+  const handleRoleChange = (event: SelectChangeEvent<unknown>) => {
+    setRole(event.target.value as string)
+  }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <TextField
-        error={errors.firstName}
-        helperText={errors.firstName?.message}
-        id="outlined"
-        label="Nombre"
-        variant="outlined"
-        {...register("firstName")}
-      />
-      <TextField
-        error={errors.lastName}
-        helperText={errors.lastName?.message}
-        id="outlined-basic"
-        label="Nombre"
-        variant="outlined"
-        {...register("lastName")}
-        />
-      <TextField
-        error={errors.age}
-        helperText={errors.age?.message}
-        id="outlined-basic"
-        label="Nombre"
-        variant="outlined"
-        type="number"
-        {...register("age")} />
-      <input type="submit" />
-    </Form>
+    <Container>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <InnerContainer>
+          <Column>
+            <InputLabel>Role</InputLabel>
+            <Selector
+              labelId="role-select-label"
+              value={role}
+              label="Role"
+              onChange={handleRoleChange}
+            >
+              <MenuItem value={'user'}>user</MenuItem>
+              <MenuItem value={'restaurant'}>restaurant</MenuItem>
+            </Selector>
+            <TextFieldStyled
+              error={errors.name}
+              helperText={errors.name?.message}
+              label="Name"
+              variant="outlined"
+              {...register('name')}
+            />
+            <TextFieldStyled
+              error={errors.email}
+              helperText={errors.email?.message}
+              label="Email"
+              variant="outlined"
+              {...register('email')}
+            />
+            <TextFieldStyled
+              error={errors.password}
+              helperText={errors.password?.message}
+              label="Password"
+              type="password"
+              variant="outlined"
+              {...register('password')}
+            />
+            <TextFieldStyled
+              error={errors.phone}
+              helperText={errors.phone?.message}
+              label="Phone"
+              variant="outlined"
+              type="number"
+              {...register('phone')}
+            />
+          </Column>
+          <LeftColumn>
+            <TextFieldStyled
+              error={errors.street}
+              helperText={errors.street?.message}
+              label="Street"
+              variant="outlined"
+              {...register('street')}
+            />
+            <TextFieldStyled
+              error={errors.streetNumber}
+              helperText={errors.streetNumber?.message}
+              label="Street Number"
+              variant="outlined"
+              {...register('streetNumber')}
+            />
+            <TextFieldStyled
+              error={errors.city}
+              helperText={errors.city?.message}
+              label="City"
+              variant="outlined"
+              {...register('city')}
+            />
+            <TextFieldStyled
+              error={errors.country}
+              helperText={errors.country?.message}
+              label="Country"
+              variant="outlined"
+              {...register('country')}
+            />
+            <TextFieldStyled
+              error={errors.postalCode}
+              helperText={errors.postalCode?.message}
+              label="Postal Code"
+              variant="outlined"
+              type="number"
+              {...register('postalCode')}
+            />
+          </LeftColumn>
+        </InnerContainer>
+        <input type="submit" />
+      </Form>
+    </Container>
   )
 }
